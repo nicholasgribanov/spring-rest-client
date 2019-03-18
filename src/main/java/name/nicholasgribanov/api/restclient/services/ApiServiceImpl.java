8dbde91caa6a52efec2cfa52e78bfe3432e4e513
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-03-18 08:17:18 +0300

    Added URI Component Builder

--- src/main/java/name/nicholasgribanov/api/restclient/services/ApiServiceImpl.java
+++ src/main/java/name/nicholasgribanov/api/restclient/services/ApiServiceImpl.java
@@ -2,22 +2,30 @@ package name.nicholasgribanov.api.restclient.services;
 
 import name.nicholasgribanov.api.domain.User;
 import name.nicholasgribanov.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @Service
 public class ApiServiceImpl implements ApiService {
     private RestTemplate restTemplate;
+    private String apiUrl;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api_url}") String apiUrl) {
         this.restTemplate = restTemplate;
+        this.apiUrl = apiUrl;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        UserData data = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromUriString(apiUrl)
+                .queryParam("limit",limit);
+
+        UserData data = restTemplate.getForObject(builder.toUriString(), UserData.class);
         return data.getData();
     }
 }
